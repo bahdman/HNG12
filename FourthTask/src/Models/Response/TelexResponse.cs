f2763a2b8855cd34ecd4c796475846fd3a7a7852
bahdman <anetoroselumese@gmail.com>
2025-02-22 16:45:19 +0100

    mod:filename

--- FourthTask/src/Models/Response/TelexResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace src.Models.Response{
-    public class TelexResponse{
-        public string message {get; set;}
-        public string username{get; set;}
-        public string event_name{get; set;}
-        public string status {get; set;}
-    }
-}
