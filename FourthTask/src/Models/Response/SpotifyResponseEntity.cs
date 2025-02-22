c02c5535c6037c3b6fcaadd04c552834a3fe7c25
Anetor Oselumese <56918389+bahdman@users.noreply.github.com>
2025-02-22 16:46:30 +0100

    Delete FourthTask/src directory

--- FourthTask/src/Models/Response/SpotifyResponseEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace src.Models.Response{
-    public class ResponseItem{
-        public string AlbumName{get; set;} = string.Empty;
-        public List<string> Artist{get; set;} = new List<string>();
-        public string ReleaseDate{get; set;} = string.Empty;
-    }
-}
